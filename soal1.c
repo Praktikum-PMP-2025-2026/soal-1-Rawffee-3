f86b6ac5fbcc4ca1fae1bccb59dc6f83752e74b0
Rafi Fathia <rafifathia@gmail.com>
2026-04-28 10:40:21 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -2,61 +2,52 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct penjaga{
+typedef struct penjaga{
     char nama[100];
     int id;
-    char shift[10];
+    char shift[50];
     int skor;
-};
+}penjaga;
 
 int comparescore(const void* a, const void* b)
 {
-    return ((struct penjaga*)a)->skor;
+    return ((struct penjaga*)a)->skor
            - ((struct penjaga*)b)->skor;
 }
 
-int main(){
+int main() {
     int kapasitas = 2; 
-    int N = 0;
+    int input;
+    int i;
+    int N;
     penjaga *daftar = (penjaga *)malloc(kapasitas * sizeof(penjaga));
+    scanf("%d", &input);
     char bufferNama[100];
     char shift[10];
-   if (N == kapasitas) {
-                kapasitas *= 2;
-                daftar = (penjaga *)realloc(daftar, kapasitas * sizeof(penjaga));
-            }
 
-            printf("Nama Lengkap: ");
-            getchar(); 
+    for(i=0; i<input; i++){
+        getchar(); 
             fgets(bufferNama, sizeof(bufferNama), stdin);
             bufferNama[strcspn(bufferNama, "\n")] = '\0'; 
 
             daftar[N].nama = (char *)malloc((strlen(bufferNama) + 1) * sizeof(char));
             strcpy(daftar[N].nama, bufferNama);
 
-            printf("ID: ");
-            scanf("%d", &daftar[N].id);
+        scanf("%d", &daftar[N].id);
 
-            printf("SHIFT: ");
-            getchar(); 
+        getchar(); 
             fgets(shift, sizeof(shift), stdin);
             shift[strcspn(shift, "\n")] = '\0'; 
 
-            daftar[N].nama = (char *)malloc((strlen(shift) + 1) * sizeof(char));
+            daftar[N].shift = (char *)malloc((strlen(shift) + 1) * sizeof(char));
             strcpy(daftar[N].nama, shift);
 
-            printf("Skor: ");
-            scanf("%d", &daftar[N].skor);
-            
-            N++;
-    int n = sizeof(daftar) / sizeof(daftar[0]);
-
-    qsort(daftar, n, sizeof(struct penjaga), comparescore);
-
-    for (int i = 0; i < n; i++) {
-        printf("%s, %d, %s, %d", daftar[i].nama,
-               daftar[i].id, daftar[i].shift, daftar[i].skor);
+        scanf("%d", &daftar[N].skor);
+    
     }
-
-    return 0;
+    int n = sizeof(daftar) / sizeof(daftar[0]);
+    qsort(daftar, n, sizeof(struct penjaga), comparescore);
+    for (int i = 0; i < n; i++) {
+        printf("%s, %d, %s, %d\n", daftar[i].nama,
+               daftar[i].id, daftar[i].shift, daftar[i].skor);
 }
